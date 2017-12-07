bb5fd485f2a52810c7bc5ce8d4bac0ba7f5d2aef
Jakob Svanholm <rymdlego@gmail.com>
2017-12-07 12:12:26 -0100

    Dependency fixes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
 	name = "kneescrape",
-	version = "0.1",
+	version = "0.13",
 	author = "Jakob Svanholm",
 	author_email = "jakob@rymdlego.se",
 	url = "https://github.com/rymdlego/kneescrape",
 	description = "A simple script to crawl a website and scrape for emails and unique words to create relevant dictionary.",
 	keywords = "crawl osint wordlist infosec",
 	scripts=['kneescrape'],
-	install_requires=['docopt', 'requests', 'bs4', 'lxml', 're']
+	install_requires=['docopt', 'requests', 'bs4']
 )
